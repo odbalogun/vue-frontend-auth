84c8b2469e69c4528759f236728715e862313990
Odun Balogun <15163838+odbalogun@users.noreply.github.com>
2023-08-06 06:00:34 +0100

    Addeed a Register component and updated styling

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '@/views/Login.vue'
+import Register from '@/views/Register.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/login', component: Login
-    }
+    { path: '/login', component: Login },
+    { path: '/register', component: Register },
   ]
 })
 
---
+++ src/views/Register.vue
@@ -0,0 +1,40 @@
+<template>
+    <main class="form-signin w-100 m-auto">
+    <form>
+      <h1 class="h3 mb-3 fw-normal">Register</h1>
+
+      <div class="form-floating">
+        <input class="form-control" placeholder="First name">
+        <label>First name</label>
+      </div>
+
+      <div class="form-floating">
+        <input class="form-control" placeholder="Last name">
+        <label>Last name</label>
+      </div>
+
+      <div class="form-floating">
+        <input type="email" class="form-control" placeholder="name@example.com">
+        <label>Email address</label>
+      </div>
+
+      <div class="form-floating">
+        <input type="password" class="form-control" placeholder="Password">
+        <label>Password</label>
+      </div>
+
+      <div class="form-floating">
+        <input type="password" class="form-control" placeholder="Confirm password">
+        <label>Confirm password</label>
+      </div>
+
+      <button class="btn btn-primary w-100 py-2 mt-3" type="submit">Register</button>
+    </form>
+  </main>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Register',
+}
+</script>
