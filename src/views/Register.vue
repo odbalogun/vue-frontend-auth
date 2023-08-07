1529b8aa70f3b9dc3f2bf169509ac65e0c966182
Odun Balogun <15163838+odbalogun@users.noreply.github.com>
2023-08-07 01:57:22 +0100

    Tied up reegistration functionality

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,30 +1,30 @@
 <template>
     <main class="form-signin w-100 m-auto">
-    <form>
+    <form @submit.prevent="submit">
       <h1 class="h3 mb-3 fw-normal">Register</h1>
 
       <div class="form-floating">
-        <input class="form-control" placeholder="First name">
+        <input v-model="data.first_name" class="form-control" placeholder="First name">
         <label>First name</label>
       </div>
 
       <div class="form-floating">
-        <input class="form-control" placeholder="Last name">
+        <input v-model="data.last_name" class="form-control" placeholder="Last name">
         <label>Last name</label>
       </div>
 
       <div class="form-floating">
-        <input type="email" class="form-control" placeholder="name@example.com">
+        <input v-model="data.email" type="email" class="form-control" placeholder="name@example.com">
         <label>Email address</label>
       </div>
 
       <div class="form-floating">
-        <input type="password" class="form-control" placeholder="Password">
+        <input v-model="data.password" type="password" class="form-control" placeholder="Password">
         <label>Password</label>
       </div>
 
       <div class="form-floating">
-        <input type="password" class="form-control" placeholder="Confirm password">
+        <input v-model="data.password_confirm" type="password" class="form-control" placeholder="Confirm password">
         <label>Confirm password</label>
       </div>
 
@@ -34,7 +34,33 @@
 </template>
 
 <script lang="ts">
+import { reactive } from 'vue';
+import axios from 'axios';
+import { useRouter } from 'vue-router';
+
 export default {
   name: 'Register',
+  setup() {
+    const data = reactive({
+      first_name: '',
+      last_name: '',
+      email: '',
+      password: '',
+      password_confirm: ''
+    });
+
+    const router = useRouter();
+
+    const submit = async () => {
+      await axios.post('http://localhost:8000/api/register', data);
+
+      await router.push('/login');
+    }
+
+    return {
+      data,
+      submit
+    }
+  }
 }
 </script>
